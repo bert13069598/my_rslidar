9fed8d8a949fbd6f7aee5911387e9787c7c60633
bert13069598 <everglow83@naver.com>
2023-09-08 16:46:13 +0900

    encoding, decoding function

--- src/filter_node.py
+++ src/filter_node.py
@@ -4,6 +4,13 @@ import rospy
 from sensor_msgs.msg import PointCloud2
 import struct
 
+def decoding(data, i, offset, axis_offset):
+    return struct.unpack('f', data[i * offset + axis_offset:i * offset + axis_offset + 4])[0]
+
+def encoding(data, axis):
+    data.extend(struct.pack('f', axis))
+    return data
+
 def callback(msg):
     # get field information from pcd msg
     fields = msg.fields
@@ -27,19 +34,19 @@ def callback(msg):
         modified_data = bytearray()
 
         for i in range(len(data)//offset):
-            x = struct.unpack('f', data[i * offset + x_offset:i * offset + x_offset + 4])[0]
-            y = struct.unpack('f', data[i * offset + y_offset:i * offset + y_offset + 4])[0]
-            z = struct.unpack('f', data[i * offset + z_offset:i * offset + z_offset + 4])[0]
-            intensity = struct.unpack('f', data[i * offset + i_offset:i * offset + i_offset + 4])[0]
+            x = decoding(data, i, offset, x_offset)
+            y = decoding(data, i, offset, y_offset)
+            z = decoding(data, i, offset, z_offset)
+            intensity = decoding(data, i, offset, i_offset)
 
             # print(x,y,z,intensity)
 
-            z *= 10
+            z *= 4
 
-            modified_data.extend(struct.pack('f', x))
-            modified_data.extend(struct.pack('f', y))
-            modified_data.extend(struct.pack('f', z))
-            modified_data.extend(struct.pack('f', intensity))
+            modified_data = encoding(modified_data, x)
+            modified_data = encoding(modified_data, y)
+            modified_data = encoding(modified_data, z)
+            modified_data = encoding(modified_data, intensity)
             
         # create new PointCloud2 data based on modified data
         pub_msg = PointCloud2()
@@ -51,11 +58,13 @@ def callback(msg):
         pub_msg.point_step = msg.point_step
         pub_msg.row_step = msg.row_step
         pub_msg.is_dense = msg.is_dense
-        pub_msg.data = bytes(modified_data)
+        pub_msg.data = modified_data
         # pub_msg.data = msg.data
 
         # publish 'filter_points' topic
         pub.publish(pub_msg)
+        # if points_num % 5 == 0:
+            # pub.publish(pub_msg)
 
 
 rospy.init_node('filter_node')
